737175ab636aa1f04b6895b16a23b04a709b4956
Diptipathak2007 <diptipathak019@gmail.com>
2025-07-26 19:18:53 +0530

    how to connect database in mern with debugging

---
+++ src/index.js
@@ -0,0 +1,37 @@
+//require("dotenv").config({path:'./env'}); hindering the code consistency
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import {DB_NAME} from "./constants.js";
+import connectDB from "./db/index.js";
+dotenv.config({path: './env'});
+
+
+
+
+
+connectDB()
+
+
+/*
+import express from "express";
+const app = express();
+
+// function connectDB(){}
+
+// connectDB()
+
+( async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`,)
+        app.on("error", () => {
+            console.error("Error occurred:", error);//listeners
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
+})()
+*/
