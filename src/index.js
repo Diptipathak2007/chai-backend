87003493cee38fbdad29e6b81f123f1140dd788e
Diptipathak2007 <diptipathak019@gmail.com>
2025-07-27 23:43:18 +0530

    created an app through express

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ dotenv.config({path: './env'});
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+})
+.catch((error => {
+    console.error("MONGO DB connection error:", error); 
+}));
 
 
 /*
